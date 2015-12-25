1cee7832b6c08f6fb6c3caed32fbce39ea1349be
Marcello Feroce <ferocemarcello@users.noreply.github.com>
2015-12-25 15:36:19 +0100

    modified classes and interfaces of controller and view so you should understand better what to do. I just did a minimal work

--- src/com/medusabookdepot/controller/Medusa.java
+++ src/com/medusabookdepot/controller/Medusa.java
@@ -3,17 +3,28 @@
  */
 package com.medusabookdepot.controller;
 
+import java.util.List;
+
+import com.medusabookdepot.model.modelInterface.Depot;
+import com.medusabookdepot.view.viewImpl.FirstFrameImpl;
+import com.medusabookdepot.view.viewinterface.FirstFrameInterface;
+
 /**
- * @author marcello
+ * @author Marcello_Feroce
  *
  */
 public class Medusa {
 
-    /**
-     * @param args
-     */
+    private final FirstFrameInterface firstframe;
+    private final List<Depot> depots;
+
+    public Medusa() {
+            this.firstframe=new FirstFrameImpl();
+            firstframe.welcome();
+            this.depots=firstframe.setInizilizationFrame();
+    }
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        new Medusa();
 
     }
 
