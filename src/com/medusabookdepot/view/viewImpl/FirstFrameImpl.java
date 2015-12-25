bddb2a882f0d225312d95151c481dd47642ee5d9
Marcello Feroce <marcello.feroce@studio.unibo.it>
2015-12-25 15:36:19 +0100

    modified classes and interfaces of controller and view so you should understand better what to do. I just did a minimal work

--- src/com/medusabookdepot/view/viewImpl/FirstFrameImpl.java
+++ src/com/medusabookdepot/view/viewImpl/FirstFrameImpl.java
@@ -3,10 +3,28 @@
  */
 package com.medusabookdepot.view.viewImpl;
 
+import java.util.List;
+
+import com.medusabookdepot.model.modelInterface.Depot;
+import com.medusabookdepot.view.viewinterface.FirstFrameInterface;
+
 /**
  * @author Marcello_Feroce
  *
  */
-public class FirstFrameImpl {
+public class FirstFrameImpl implements FirstFrameInterface{
+
+    @Override
+    public void welcome() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<Depot> setInizilizationFrame() {
+        return null;
+        // TODO Auto-generated method stub
+        
+    }
 
 }
--- src/com/medusabookdepot/view/viewinterface/FirstFrameInterface.java
+++ src/com/medusabookdepot/view/viewinterface/FirstFrameInterface.java
@@ -3,10 +3,18 @@
  */
 package com.medusabookdepot.view.viewinterface;
 
+import java.util.List;
+
+import com.medusabookdepot.model.modelInterface.Depot;
+
 /**
  * @author Marcello_Feroce
  *
  */
 public interface FirstFrameInterface {
+
+    void welcome();
+
+    List<Depot> setInizilizationFrame();
 
 }
