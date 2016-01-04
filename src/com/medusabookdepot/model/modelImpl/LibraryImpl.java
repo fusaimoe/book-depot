2e2251c9db5b3cc3bbc2ae67e31f0f003b49ba78
Marcello Feroce <ferocemarcello@users.noreply.github.com>
2016-01-04 10:54:40 +0100

    optimized code so that there is a new method present in superclass to return a new tracking number

--- src/com/medusabookdepot/model/modelImpl/LibraryImpl.java
+++ src/com/medusabookdepot/model/modelImpl/LibraryImpl.java
@@ -27,16 +27,7 @@ public class LibraryImpl extends CustomerImpl implements Library{
 
     @Override
     public void doTransfer(Transferrer opposite, boolean sender,Map<StandardBook,Integer> books) {
-        List<String>allTrackings=new ArrayList<>();
-        for(Transfer trans:transfers){
-            allTrackings.add(trans.getTrackingNumber());
-        }
-        Random rm=new Random();
-        String tr=String.valueOf(rm.nextInt(1000000));
-        while(!allTrackings.contains(tr)) {
-            tr=String.valueOf(rm.nextInt(1000000));
-        }
-        transfers.add(new TransferImpl(sender? this:opposite, sender==false? opposite:this, Date.valueOf(LocalDate.now()),tr,books));
+        transfers.add(new TransferImpl(sender? this:opposite, sender==false? opposite:this, Date.valueOf(LocalDate.now()),this.getNewTrackingNumber(),books));
         
     }
     public String toString() {
