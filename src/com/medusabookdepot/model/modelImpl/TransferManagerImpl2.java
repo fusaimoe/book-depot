307099207e03086f65772591e5a61267b84a445e
Marcello Feroce <ferocemarcello@users.noreply.github.com>
2016-01-30 08:55:50 +0100

    add removeTransfer methods in TransferManagerImpl2

--- src/com/medusabookdepot/model/modelImpl/TransferManagerImpl2.java
+++ src/com/medusabookdepot/model/modelImpl/TransferManagerImpl2.java
@@ -50,6 +50,18 @@ public class TransferManagerImpl2 implements TransferManager {
         this.writeTransferOnFile("trasferimenti.txt",transfer);
         
     }
+    @Override
+    public void removeTransfer(Transfer transfer) {
+        this.transfers.remove(transfer);
+        this.removeTransferFromFile("trasferimenti.txt",transfer);
+        
+    }
+    @Override
+    public void removeTransfer(int index) {
+        this.transfers.remove(index);
+        this.removeTransferFromFile("trasferimenti.txt",index);
+        
+    }
 
     private void writeTransferOnFile(String fileName, Transfer transfer) {
         try {
@@ -66,6 +78,34 @@ public class TransferManagerImpl2 implements TransferManager {
         
     }
 
+    private void removeTransferFromFile(String fileName, Transfer transfer) {
+        try {
+            List<Transfer> trans=getTransfersFromFile(fileName);
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(getFilePath(fileName)));
+            trans.remove(transfer);
+            oos.writeObject(trans);
+            oos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    private void removeTransferFromFile(String fileName, int index) {
+        try {
+            List<Transfer> trans=getTransfersFromFile(fileName);
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(getFilePath(fileName)));
+            trans.remove(index);
+            oos.writeObject(trans);
+            oos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
     private String getFilePath(String string) {
         String filepath = System.getProperty("user.home")+System.getProperty("file.separator")+ string;
         return filepath;
@@ -113,15 +153,22 @@ public class TransferManagerImpl2 implements TransferManager {
         
         Map<StandardBook, Integer>mm2=new HashMap<>();
         mm2.put(new StandardBookImpl("evdfb ", "caselli_merda", 2040, 20,"mate", "calcolomer", "gesu", 234), Integer.valueOf(8));
-        mm2.put(new StandardBookImpl("eerdfs ", "pianini merda", 2051, 50,"labo", "oopmerd", "dio", 400), Integer.valueOf(10));
+        mm2.put(new StandardBookImpl("eerdfs ", "pianini merda", 2051, 50,"labo", "oopmerd", "dio", 400), Integer.valueOf(20));
         Transferrer tra2=new PrinterImpl("printer", "via del vaffa 2", "07184939");
         Transferrer trad2=new DepotImpl("sw", mm2);
-        Transfer tr2=new TransferImpl(tra2, trad2, new Date(1997, 4,8), mm, "21321");
+        Transfer tr2=new TransferImpl(tra2, trad2, new Date(1997, 4,8), mm2);
         
         TransferManagerImpl2.getInstanceOfTransferManger().addTransfer(tr);
         TransferManagerImpl2.getInstanceOfTransferManger().addTransfer(tr2);
 
-        System.out.println(TransferManagerImpl2.getInstanceOfTransferManger().getTransfersFromFile("trasferimenti.txt").get(1));
+        System.out.println(TransferManagerImpl2.getInstanceOfTransferManger().getAllTransfers().get(0).getQuantity());
+        System.out.println(TransferManagerImpl2.getInstanceOfTransferManger().getAllTransfers().get(1).getQuantity());
+        System.out.println(TransferManagerImpl2.getInstanceOfTransferManger().getAllTransfers().size());
+        TransferManagerImpl2.getInstanceOfTransferManger().removeTransfer(0);
+        System.out.println(TransferManagerImpl2.getInstanceOfTransferManger().getAllTransfers().get(0).getQuantity());
+        System.out.println(TransferManagerImpl2.getInstanceOfTransferManger().getAllTransfers().size());
+
+
     }
 
 }
