cd49e4ecd609e269b83bfc493af0488db54d1a07
Marcello Feroce <marcello.feroce@studio.unibo.it>
2016-02-02 09:51:32 +0100

    implemented using javafx, customer Class, and all its extensions, i left booleans as they were

--- src/com/medusabookdepot/model/modelImpl/CustomerImpl.java
+++ src/com/medusabookdepot/model/modelImpl/CustomerImpl.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 
 import com.medusabookdepot.model.modelInterface.Customer;
 
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 /**
@@ -19,45 +20,46 @@ public abstract class CustomerImpl extends TransferrerImpl implements Customer,S
      * 
      */
     private static final long serialVersionUID = -9063812037630746920L;
-    protected String address;
-    protected String telephoneNumber;
+    protected StringProperty address;
+    protected StringProperty telephoneNumber;
     protected boolean isHuman;
     protected boolean isLibrary;
     public CustomerImpl(String name,String address, String telephoneNumber) {
         super(name);
-        this.address=address;
-        this.telephoneNumber=telephoneNumber;
+        this.address=new SimpleStringProperty(address);
+        this.telephoneNumber=new SimpleStringProperty(telephoneNumber);
         this.isDepot=false;
     }
     @Override
     public String getAddress() {
-        return this.address;
+        return this.getAddressProperty().get();
     }
 
     @Override
     public String getTelephoneNumber() {
-        return this.telephoneNumber;
+        return this.getTelephoneNumberProperty().get();
     }
 
-    @Override
-    public void setAddress(String address) {
-        this.address=address;
-    }
-
-    @Override
-    public void setTelephoneNumber(String telephoneNumber) {
-        this.telephoneNumber=telephoneNumber;
-    }
     @Override
     public StringProperty getAddressProperty() {
-        return null;
+        return this.address;
         
     }
     @Override
     public StringProperty getTelephoneNumberProperty() {
-        return null;
+        return this.telephoneNumber;
         
     }
+    @Override
+    public void setAddress(String address) {
+        this.address.set(address);
+    }
+
+    @Override
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber.set(telephoneNumber);
+    }
+    
     
     @Override
     abstract public String toString();
