0a7856321770aa0f15c6e8859d3caeae8c28a6ca
Marcello Feroce <ferocemarcello@users.noreply.github.com>
2016-02-15 15:02:02 +0100

    modified TestModel class, and created method getIsbns, and getStandardBooksAsList

--- src/com/medusabookdepot/model/modelImpl/TestModel.java
+++ src/com/medusabookdepot/model/modelImpl/TestModel.java
@@ -3,11 +3,15 @@ package com.medusabookdepot.model.modelImpl;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import com.medusabookdepot.model.modelInterface.CanSendTransferrer;
+import com.medusabookdepot.model.modelInterface.Customer;
 import com.medusabookdepot.model.modelInterface.Depot;
 import com.medusabookdepot.model.modelInterface.StandardBook;
 import com.medusabookdepot.model.modelInterface.Transfer;
@@ -16,31 +20,27 @@ import com.medusabookdepot.model.modelInterface.Transferrer;
 public class TestModel {
 
     @org.junit.Test
-    public void testTransfers() {/*
+    public void testTransfers() {
         Map<StandardBook, Integer> mm = new HashMap<>();
-        StandardBook b=new StandardBookImpl("iiiinb", "eeee", 2010, 43, "infoblew", "sisos", "io", 23);
-        StandardBook b2=new StandardBookImpl("iiiissnb", "fff", 2011, 32, "infoblew", "oop", "io", 40);
+        StandardBook b=new StandardBookImpl("iiiinb", "il fantasma", 2010, 43, "la casa degli spettri", "orrore", "stephen King", 23);
+        StandardBook b2=new StandardBookImpl("iiiissnb", "l'orso nella casa blu", 2011, 32, "bimbi cattivi", "infanzia rovinata", "quel mattachione dell'orso bear", 40);
         mm.put(b, Integer.valueOf(5));
         mm.put(b2, Integer.valueOf(9));
-        Transferrer per = new PersonImpl("joy", "via lazio 4", "333 332 332");
-        Depot trad = new DepotImpl("D1", mm);
-        DepotManagerImpl.getInstanceOfDepotManger().addDepot(trad);
-        Calendar cal = Calendar.getInstance();
-        cal.set(2013, 2, 2);
-        List<String> isbns = new ArrayList<>();
-        isbns.add("iiiinb");
-        Map<StandardBook, Integer> mapps = trad.getBooksFromStandardBookIsbn(isbns);
-        assertTrue(trad.containsBooks(mapps));
-        assertTrue(mapps.get(b)==5);
-        assertTrue(mapps.size()==1);
-        List<Pair<String, Integer>> lis = new ArrayList<>();
-        lis.add(new Pair<String, Integer>("iiiinb", 2));
-        lis.add(new Pair<String, Integer>("iiiissnb", 6));
-        mapps = trad.getBooksFromStandardBookIsbnAndQuantity(lis);
-        assertTrue(mapps.get(b)==2&&mapps.get(b2)==6);
-        assertTrue(mapps.size() == 2);
-        Transfer tr = new TransferImpl(trad, per, cal.getTime(), mapps, "883737");
-        TransferManagerImpl.getInstanceOfTransferManger().addTransfer(tr);*/
+        Depot dep = new DepotImpl("Jellyfish", mm);
+        assertTrue(dep.isADepot());
+        assertTrue(dep.containsBooks(mm));
+        assertTrue(dep.getBooks().equals(mm));
+        System.out.println(dep);
+        Customer printo=new PrinterImpl("non solo stampe", "via della carta 3", "0712074248");
+        assertTrue(printo.getName().equals("non solo stampe")&&printo.getAddress().equals("via della carta 3")&&printo.getTelephoneNumber().equals("0712074248"));
+        assertTrue(!printo.isADepot());
+        assertTrue(printo.isAPrinter()&&!printo.isAPerson()&&!printo.isALibrary());
+        List<StandardBook>lis=new ArrayList<>();
+        lis=dep.getStandardBooksAsList();
+        List<String>lisIsbn=new ArrayList<>();
+        lisIsbn.add(lis.get(0).getIsbn());
+        dep.removeBooks(dep.getBooksFromStandardBookIsbn(lisIsbn));
+        assertTrue(dep.getBooks().size()==1);
     }
 
 }
