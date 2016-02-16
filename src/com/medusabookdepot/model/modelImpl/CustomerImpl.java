153b57ef2ad9f37017b5fd523a92706dee04e372
Marcello Feroce <marcello.feroce@studio.unibo.it>
2016-02-16 16:02:16 +0100

    done 3.2 of relation

--- src/com/medusabookdepot/model/modelImpl/CustomerImpl.java
+++ src/com/medusabookdepot/model/modelImpl/CustomerImpl.java
@@ -11,6 +11,9 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 
 /**
+ * Abstract class that implements Customer interface, and extends the abstract
+ * class TransferrerImpl, which implements Transferrer
+ * 
  * @author Marcello_Feroce
  *
  */
@@ -25,47 +28,82 @@ public abstract class CustomerImpl extends TransferrerImpl implements Customer, 
     protected StringProperty telephoneNumber;
     public CustomerImpl(String name, String address, String telephoneNumber) {
         super(name);
-        this.address = new SimpleStringProperty(address);
+        this.address = new SimpleStringProperty(address);// qui e subito sotto
+                                                         // mi creo un altro
+                                                         // oggetto, così da non
+                                                         // mantenere il
+                                                         // riferimento con gli
+                                                         // oggetti in input
         this.telephoneNumber = new SimpleStringProperty(telephoneNumber);
     }
+    /**
+     * get the address of the customer, using a defensive copy
+     */
     @Override
     public String getAddress() {
-        String addr=new String(this.address.get());//copia difensiva
+        String addr = new String(this.address.get());// copia difensiva
         return addr;
     }
-
+    /**
+     * get the telephone number of the customer, using a defensive copy
+     */
     @Override
     public String getTelephoneNumber() {
-        String tel=new String(this.telephoneNumber.get());//copia difensiva
+        String tel = new String(this.telephoneNumber.get());// copia difensiva
         return tel;
     }
+    /**
+     * set the address of the customer, creating a new object, so doesn't keep the referement with the input
+     */
     @Override
     public void setAddress(String address) {
-        this.address.set(address);
+        this.address.set(new String(address));
     }
-
+    /**
+     * set the telephone number of the customer, creating a new object, so doesn't keep the referement with the input
+     */
     @Override
     public void setTelephoneNumber(String telephoneNumber) {
-        this.telephoneNumber.set(telephoneNumber);
+        this.telephoneNumber.set(new String(telephoneNumber));
     }
+    /**
+     * always returns false, because a Customer is not a Depot
+     */
     @Override
     public boolean isADepot() {
         return false;
     }
+    /**
+     * abstract method, return true if the Customer object is istance of Printer
+     */
     @Override
     public abstract boolean isAPrinter();
+    /**
+     * abstract method, return true if the Customer object is istance of Person
+     */
     @Override
     public abstract boolean isAPerson();
+    /**
+     * abstract method, return true if the Customer object is istance of Library
+     */
     @Override
     public abstract boolean isALibrary();
+    /**
+     * return the address as StringProperty
+     */
     @Override
     public StringProperty addressProperty() {
-        StringProperty addr=new SimpleStringProperty(this.address.get());//copia difensiva
+        StringProperty addr = new SimpleStringProperty(this.address.get());// copia
+                                                                           // difensiva
         return addr;
     }
+    /**
+     * return the telephone number as StringProperty
+     */
     @Override
     public StringProperty telephoneNumberProperty() {
-        StringProperty tel=new SimpleStringProperty(this.telephoneNumber.get());//copia difensiva
+        StringProperty tel = new SimpleStringProperty(this.telephoneNumber.get());// copia
+                                                                                  // difensiva
         return tel;
     }
     @Override
