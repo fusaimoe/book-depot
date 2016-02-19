06fa22c77035b00ad94e928d452f14cfa1d741f8
Andrea Colombo <andread251@gmail.com>
2016-02-20 00:30:46 +0100

    Wrapper and FileManager are now generic and no more specific to books only. Converted PDFs now have current date in their name.

--- src/com/medusabookdepot/controller/files/FileManager.java
+++ src/com/medusabookdepot/controller/files/FileManager.java
@@ -1,101 +1,114 @@
 package com.medusabookdepot.controller.files;
 
 import java.io.File;
+import java.util.List;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-
-import com.medusabookdepot.model.modelImpl.StandardBookImpl;
-import com.medusabookdepot.model.modelImpl.StandardBookListWrapper;
+import javax.xml.namespace.QName;
+import javax.xml.transform.stream.StreamSource;
 
 import javafx.collections.ObservableList;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
 
-public class FileManager {
+public class FileManager<A> {
 
 	/* Passo tramite costruttore l'oggetto BooksController sulla cui lista la view agisce;
 	* però per rendere questa classe utilizzabile non solo coi Book, dovrò realizzare un
 	* interfaccia del controller che venga poi implementata da tutti i tipi diversi di
 	* controller (tipo MovementsController, LibraryController, DepotController ecc.)
 	*/
-	private ObservableList<StandardBookImpl> books;
+	private List<A> list;
 	private File file;
+	private QName qName;
+	private final Class<A> classType;
 	
 	/**
 	 * Constructor
-	 * It makes the directory "book-depot" in home,
+	 * It creates the directory "book-depot" in home,
 	 * load the file, if it exists and if it's not empty
 	 * 
+	 * @param list
+	 * List of elements to manage from and to the file 
 	 * @param filePath
+	 * Xml file to manage
+	 * @param classType
+	 * Class of the elements of the file to manage
+	 * @param qName
+	 * Name for the list of element in the XML file.
+	 * IE books (for element book of StandardBookImpl class), customers (for element customer of CustomerImpl class), ecc.
+	 * It's needed only for XML file saving as XML loading accepts any element as root
 	 */
-	public FileManager(ObservableList<StandardBookImpl> books, String filePath) {
+	public FileManager(ObservableList<A> list, String filePath, Class<A> classType, String qName) {
 		super();
-		this.books = books;
+		this.list = list;
+		this.classType = classType;
+		
+		this.qName= new QName(qName);
 		
 		file = new File(filePath);	
-		// Make the directory "book-depot" in home
+		// Make the directory "book-depot" in home if doesn't exist already
 		file.getParentFile().mkdirs();
 		// Load the file, if it exists and if it's not empty
-		if(file.exists() && file.length()>65){
+		if(file.exists() && file.length()!=0){
 			loadDataFromFile();
 		}
 		
 	}
+	
 	/**
-	 * Loads person data from the specified file. The current person data will
-	 * be replaced.
-	 * 
-	 * @param file
+	 * Loads element data from the file specified in the constructor. The current element data will be replaced.
 	 */
 	public void loadDataFromFile() {
 	    try {
-	        JAXBContext context = JAXBContext.newInstance(StandardBookListWrapper.class);
+	        JAXBContext context = JAXBContext.newInstance(Wrapper.class, this.classType);
 	        Unmarshaller um = context.createUnmarshaller();
-
-	        // Reading XML from the file and unmarshalling.
-	        StandardBookListWrapper wrapper = (StandardBookListWrapper) um.unmarshal(file);
-
-	        books.clear();
-	        books.addAll(wrapper.getBooks());
 	        
-	    } catch (Exception e) { // catches ANY exception
-	        Alert alert = new Alert(AlertType.ERROR);
-	        alert.setTitle("Error");
-	        alert.setHeaderText("Could not load data from file:");
-	        alert.setContentText(file.getPath());
-	        alert.getDialogPane().getStylesheets().add(getClass().getResource("materialDesign.css").toExternalForm());
-
-	        alert.showAndWait();
+	        // Clearing the list before reading elements
+	        list.clear();
+	        
+	        // Reading XML from the file and unmarshalling
+	        list.addAll(unmarshal(um, this.classType, file.getPath()));
+	        
+	    } catch (Exception e) {
+	        e.printStackTrace();
 	    }
 	}
+	
+	/**
+     * Unmarshal XML to Wrapper and return List value.
+     */
+	@SuppressWarnings("unchecked")
+	private static <A> List<A> unmarshal(Unmarshaller unmarshaller, Class<A> clazz, String xmlLocation) throws JAXBException {
+        StreamSource xml = new StreamSource(xmlLocation);
+        Wrapper<A> wrapper = (Wrapper<A>) unmarshaller.unmarshal(xml, Wrapper.class).getValue();
+        return wrapper.getElements();
+    }
 
 	/**
-	 * Saves the current person data to the specified file.
-	 * 
-	 * @param file
+	 * Saves the current person data to the file specified in the constructor.
 	 */
+	@SuppressWarnings("rawtypes")
 	public void saveDataToFile() {
 	    try {
-	        JAXBContext context = JAXBContext.newInstance(StandardBookListWrapper.class);
+	        JAXBContext context = JAXBContext.newInstance(Wrapper.class, this.classType);
 	        Marshaller m = context.createMarshaller();
 	        m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
-	        // Wrapping our person data.
-	        StandardBookListWrapper wrapper = new StandardBookListWrapper();
-	        wrapper.setBooks(books);
+	        // Wrapping our element data
+	        Wrapper<A> wrapper = new Wrapper<>();
+	        wrapper.setElements(list);
+	        
+	        // Leverage JAXBElement to supply root element information
+	        JAXBElement<Wrapper> jaxbElement = new JAXBElement<Wrapper>(this.qName, Wrapper.class, wrapper);
 
-	        // Marshalling and saving XML to the file.
-	        m.marshal(wrapper, file);
+	        // Marshalling and saving XML to the file
+	        m.marshal(jaxbElement, file);
 
-	    } catch (Exception e) { // catches ANY exception
-	        Alert alert = new Alert(AlertType.ERROR);
-	        alert.setTitle("Error");
-	        alert.setHeaderText("Could not save data to file:");
-	        alert.setContentText(file.getPath());
-	        alert.getDialogPane().getStylesheets().add(getClass().getResource("materialDesign.css").toExternalForm());
-	        alert.showAndWait();
+	    } catch (Exception e) {
+	        e.printStackTrace();
 	    }
 	}
 	
