a0b75fd23b0ceea66688280166c04313c6a493b4
Giulia Cecchetti <giulia.cecchetti2@studio.unibo.it>
2016-02-21 15:51:57 +0100

    'AddDepot.fxml' page and control. Now Depots can be added, modified and deleted. Added 'search' option to addDepot, addBook, addCostumer, Books and Costumers pages. Other screens updated.

--- src/com/medusabookdepot/view/MovementsControl.java
+++ src/com/medusabookdepot/view/MovementsControl.java
@@ -4,10 +4,16 @@
 
 package com.medusabookdepot.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class MovementsControl extends ScreenControl{
 	
 	public MovementsControl(){
 		super();
 	}
+	
+	@FXML
+    private TextField searchField;
 	
 }
