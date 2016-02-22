377886f85b2c8d1b738cb69614a96bf4bda25063
Andrea Colombo <andread251@gmail.com>
2016-02-22 22:05:47 +0100

    Depot screen is now fully functional. Depots can be filtered using the buttons at the top.

--- src/com/medusabookdepot/view/DepotsControl.java
+++ src/com/medusabookdepot/view/DepotsControl.java
@@ -1,5 +1,7 @@
 package com.medusabookdepot.view;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map.Entry;
 
 import com.medusabookdepot.controller.DepotsController;
@@ -7,6 +9,8 @@ import com.medusabookdepot.model.modelImpl.DepotImpl;
 import com.medusabookdepot.model.modelImpl.StandardBookImpl;
 
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -14,17 +18,39 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.layout.HBox;
 
 public class DepotsControl extends ScreenControl {
 	
+	// Reference to the controller
 	private final DepotsController depotsController = new DepotsController();
+	
+	// Aler panel to manage exceptions
     private final Alert alert = new Alert(AlertType.WARNING);
+    
+    // List of books & quantity to view in the table
+    private final ObservableList<Entry<StandardBookImpl,Integer>> data = FXCollections.observableArrayList();
+    
+    // ToggleGroup to have just one toggleButton selected at a time
+    private final ToggleGroup buttonsGroup = new ToggleGroup();
+    private final List<ToggleButton> buttonsList = new ArrayList<>();
     
 	public DepotsControl(){
 		super();
+		
+		// Creating a button for each depot and adding such button to the group and to the list
+		for(DepotImpl depot : depotsController.getDepots()){
+			ToggleButton button = new ToggleButton(depot.getName());
+			button.setToggleGroup(buttonsGroup);
+			buttonsList.add(button);
+			button.setUserData(depot.getName());
+		}
+		
+		// Adding CSS to the Alert panel
 		alert.getDialogPane().getStylesheets().add(getClass().getResource("materialDesign.css").toExternalForm());
 	}
 	
@@ -58,15 +84,14 @@ public class DepotsControl extends ScreenControl {
 
 	public void initialize() {
 		
-		final ObservableList<Entry<StandardBookImpl,Integer>> data = FXCollections.observableArrayList();
-		
-		int i=2;
-		for(DepotImpl depot : depotsController.getDepots()){
-			ToggleButton button = new ToggleButton(depot.getName());
-			hBox.getChildren().add(i, button);
-			i++;
+		// Adding the buttons created in the constructor to the hBox, after the title
+		int hBoxPos=2; // hBoxPos is 2 because I need to add buttons after the title, which is in pos 1
+		for(ToggleButton button : buttonsList){	
+			hBox.getChildren().add(hBoxPos, button);
+			hBoxPos++; // the other buttons will be added after the one I just added
 		}
 		
+		// Initializing the table
         quantityColumn.setCellValueFactory(cellData ->  new ReadOnlyStringWrapper(cellData.getValue().getValue().toString()));
         isbnColumn.setCellValueFactory(cellData -> cellData.getValue().getKey().isbnProperty());
         titleColumn.setCellValueFactory(cellData -> cellData.getValue().getKey().titleProperty());
@@ -77,13 +102,34 @@ public class DepotsControl extends ScreenControl {
         authorColumn.setCellValueFactory(cellData -> cellData.getValue().getKey().authorProperty());
         priceColumn.setCellValueFactory(cellData -> cellData.getValue().getKey().priceProperty().asString());
 		
-        //Lo zero serve per indicare di quale depot deve mostrare i libri. In questo caso mostra solo i libri del depot  
-        // che si trova in posizione 0 della lista. Ovvero il primo depot che ho aggiunto. 
-        //Lo zero dovrà essere modificato in modo tale che mostri solo i libri del depot indicato dal bottone che l'utente
-        //ha premuto. Al momento non ho idee.
-        data.addAll(depotsController.getDepots().get(0).getBooks().entrySet());
+        // Method which handle the selection of a depot
+        this.filter();
         
+        // Selecting the first depot of the list for the first time the user opens the screen
+        buttonsList.get(0).setSelected(true);
+        
+        // Putting data into the table
         depotsTable.setItems(data);
+        
 	}
 
+	/**
+	 * Called when the user selects a toggle-button, it filters depots so in the table there are only the books from the selected depot
+	 */
+	private void filter(){
+		
+		buttonsGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>(){
+			public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+				if(newValue==null){
+					// When there are no buttons toggled, the table is empty
+					data.clear();
+				} else{
+					// The table is cleaned
+					data.clear();
+					// Data is added to the table, relatively to the toggle button selected
+					data.addAll(depotsController.searchDepot((String)buttonsGroup.getSelectedToggle().getUserData()).findFirst().get().getBooks().entrySet());
+				}
+			}
+		});
+	}
 }
